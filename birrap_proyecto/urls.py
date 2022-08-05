8a1881f3719ea07f708811c1b9d6ccff9288ef43
Claudio Rodriguez <claudio.rodriguez1992@gmail.com>
2022-08-05 02:21:19 -0300

    creacion new_product

--- birrap_proyecto/urls.py
+++ birrap_proyecto/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from birrap_proyecto.views import template_base
+from products.views import create_product
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("template_base/", template_base, name='template_base'),
+    path('create_products/', create_product, name='create_products'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from products.models import Products
 
-# Create your views here.
+def create_product(request):
+    new_product = Products.objects.create(
+        name = "Golden Argenta",
+        style = "Ale",
+        alcohol_volume = 5,
+        IBU = 3,
+        description = "Esta cerveza Rubia de estilo ingles, muy balanceada, ""\n""de color dorado y excelente cuerpo gracias a la combinación ""\n""de maltas pálidas y maltas acarameladas.",
+        malt = "Pálida",
+        hop = "Cascade",
+        price = 480,
+        creation_date = "", 
+        is_active = True,
+    )
+    context = {
+        "new_product" : new_product
+    }
+    return render(request,"new_product.html", context=context)
