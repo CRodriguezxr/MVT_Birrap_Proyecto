c96dbe49fa2da92ffafdd217ec58c7eb05e7fd48
Claudio Rodriguez <claudio.rodriguez1992@gmail.com>
2022-08-05 01:52:40 -0300

    creacion clase producto

--- birrap_proyecto/views.py
+++ birrap_proyecto/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
 
 
 
 def template_base(request):
-    return render(request, "template_base.html", context={})
+    context = {
+
+    }
+    return render(request, "template_base.html", context=context)
---
+++ products/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Products(models.Model):
+    name = models.CharField (max_length=50,null=True,blank=True)
+    style = models.CharField (max_length=50,null=True,blank=True)
+    alcohol_volume = models.FloatField (max_length=2,null=True,blank=True)
+    IBU = models.FloatField (max_length=2,null=True,blank=True)
+    description = models.CharField (max_length=500,null=True,blank=True)
+    malt = models.CharField (max_length=50,null=True,blank=True)
+    hop = models.CharField (max_length=50,null=True,blank=True)
+    price = models.FloatField (default=True, null=True,blank=True)
+    creation_date = models.DateField (auto_now_add=True, null=True, blank=True)
+    is_active = models.BooleanField (default=True)
+
+class Category(models.Model):
+    name = models.CharField (max_length=255)
