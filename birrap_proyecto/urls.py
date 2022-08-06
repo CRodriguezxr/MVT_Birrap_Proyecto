e6fc8a572e25a3949acff9d6fff802f6768ffb20
Claudio Rodriguez <claudio.rodriguez1992@gmail.com>
2022-08-06 09:02:14 -0300

    Modificacion html new_product

--- birrap_proyecto/urls.py
+++ birrap_proyecto/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from birrap_proyecto.views import template_base
-from products.views import create_product
+from products.views import create_product, list_products
 
 
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("template_base/", template_base, name='template_base'),
     path('create_products/', create_product, name='create_products'),
+    path("list_products/", list_products, name='list_products'),
 ]
--- products/views.py
+++ products/views.py
@@ -3,13 +3,13 @@ from products.models import Products
 
 def create_product(request):
     new_product = Products.objects.create(
-        name = "Golden Argenta",
-        style = "Ale",
-        alcohol_volume = 5,
-        IBU = 3,
-        description = "Esta cerveza Rubia de estilo ingles, muy balanceada, ""\n""de color dorado y excelente cuerpo gracias a la combinación ""\n""de maltas pálidas y maltas acarameladas.",
-        malt = "Pálida",
-        hop = "Cascade",
+        name = "Amber Lager",
+        style = "Lager",
+        alcohol_volume = 6,
+        IBU = 4,
+        description = "Es una cerveza con una combinación de lúpulos patagónicos y un blend de finas maltas que generan su color rojizo,un delicado aroma y un amargor apacible que permite dar a luz a un tostado delicioso./n Ideal para acompañar carnes asadas y pastas con salsas especiadas..",
+        malt = "Caramelo",
+        hop = "Hercules",
         price = 480,
         creation_date = "", 
         is_active = True,
@@ -18,3 +18,10 @@ def create_product(request):
         "new_product" : new_product
     }
     return render(request,"new_product.html", context=context)
+
+def list_products(request):
+    products = Products.objects.all()
+    context = {
+        "products" : products
+    }
+    return render(request,"products_list.html", context=context)
