702f826e92f7b4275da8a0986274f3fa6ca3cc6d
Claudio Rodriguez <claudio.rodriguez1992@gmail.com>
2022-08-06 10:43:35 -0300

    modificación en URLS - creación urls.py en carpeta products - extend

--- birrap_proyecto/urls.py
+++ birrap_proyecto/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from birrap_proyecto.views import template_base
 from products.views import create_product, products_list
 
@@ -8,6 +8,5 @@ from products.views import create_product, products_list
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("template_base/", template_base, name='template_base'),
-    path('create_products/', create_product, name='create_products'),
-    path("products_list/", products_list, name='products_list'),
+    path("products/", include("products.urls"))
 ]
---
+++ products/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from products.views import products_list, create_product
+
+urlpatterns = [
+    path('create_products/', create_product, name='create_products'),
+    path("products_list/", products_list, name='products_list'),
+]
